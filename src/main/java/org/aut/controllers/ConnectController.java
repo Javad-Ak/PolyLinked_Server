f6e4a5d29c0dc67ede9c84c440c48e8e0eed0d43
Alireza Atharifard <a.atharifard1384@gmail.com>
2024-05-31 11:44:23 +0330

    some logical improvements in cannectController

--- src/main/java/org/aut/controllers/ConnectController.java
+++ src/main/java/org/aut/controllers/ConnectController.java
@@ -13,20 +13,21 @@ public class ConnectController {
             throw new NotAcceptableException("Null connect");
         } else if (!UserController.userExistsById(connect.getApplicant_id()) || !UserController.userExistsById(connect.getAcceptor_id())) {
             throw new NotFoundException("User not found");
-        } else if (ConnectAccessor.getAcceptedConnect(connect.getAcceptor_id() , connect.getApplicant_id()) != null || ConnectAccessor.getWaitingConnectOfAcceptor(connect.getApplicant_id(), connect.getAcceptor_id()) != null) {
+        } else if (ConnectAccessor.getWaitingConnectOfAcceptor(connect.getApplicant_id(), connect.getAcceptor_id()) != null) {
             throw new NotAcceptableException("Connect already exists");
         } else if (connect.getAcceptor_id().equals(connect.getApplicant_id())) {
             throw new NotAcceptableException("User can't connect to himself");
         }
         Connect waitingConnect;
-        if ((waitingConnect = ConnectAccessor.getWaitingConnectOfAcceptor(connect.getApplicant_id(), connect.getAcceptor_id())) != null) {
+        if ((waitingConnect = ConnectAccessor.getWaitingConnectOfAcceptor(connect.getAcceptor_id(), connect.getApplicant_id())) != null) {
             updateConnect(new Connect(waitingConnect.getApplicant_id(), waitingConnect.getAcceptor_id(), waitingConnect.getNote(), Connect.AcceptState.ACCEPTED));
         }
         ConnectAccessor.addConnect(connect);
     }
 
     public static void updateConnect(Connect connect) throws SQLException, NotFoundException , NotAcceptableException {
-        if ((ConnectAccessor.getWaitingConnectOfAcceptor(connect.getApplicant_id() , connect.getAcceptor_id()) != null && (connect.getAccept_state().equals("ACCEPTED") || connect.getAccept_state().equals("REJECTED")))) {
+        if ((ConnectAccessor.getWaitingConnectOfAcceptor(connect.getApplicant_id() , connect.getAcceptor_id()) != null &&
+                (connect.getAccept_state().equals("ACCEPTED") || connect.getAccept_state().equals("REJECTED")))) {
             ConnectAccessor.updateConnect(connect);
         } else {
             throw new NotFoundException("Connect not found");
